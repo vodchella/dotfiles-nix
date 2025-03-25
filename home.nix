436988b33e13766fd1e83a27feca07e6399e5dbc
vodchella <twister.kz@gmail.com>
2025-03-25 03:54:46 +0000

    Modify file structure

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./programs/all.nix
+  ];
+
   home.username = "vodchella";
   home.homeDirectory = "/home/vodchella";
 
@@ -29,66 +33,5 @@
     r ="ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
     v="nvim";
     z="zellij";
-  };
-
-  programs = {
-    fish = {
-      enable = true;
-      package = pkgs.fish.overrideAttrs (oldAttrs: {
-        version = "3.7.1";
-        src = pkgs.fetchurl {
-          url = "https://github.com/fish-shell/fish-shell/releases/download/3.7.1/fish-3.7.1.tar.xz";
-          sha256 = "614c9f5643cd0799df391395fa6bbc3649427bb839722ce3b114d3bbc1a3b250";
-        };
-        patches = [ ];
-	doCheck = false;
-	doInstallCheck = false;
-	doTest = false;
-	buildInputs = oldAttrs.buildInputs ++ [ pkgs.ncurses ];
-	patchPhase = ''
-        echo "Skipping patchPhase"
-        '';
-        checkPhase = ''
-          echo "Skipping checkPhase"
-        '';
-      });
-      shellInit = ''
-        set -gx fish_greeting 'Hello, Max!'
-        set -gx LANG en_US.UTF-8
-        set -gx LC_ALL en_US.UTF-8
-
-        set -gx PATH $PATH $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin
-        set -gx NIX_PATH "nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs"
-        set -gx MANPATH $MANPATH $HOME/.nix-profile/share/man
-        set -gx INFOPATH $INFOPATH $HOME/.nix-profile/share/info
-        
-        if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-            source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-        end
-      '';
-      interactiveShellInit = ''
-      set -gx PATH $PATH $HOME/.nix-profile/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin
-
-      if not pgrep -u (id -u) ssh-agent > /dev/null
-          eval (ssh-agent -c) >/dev/null
-      end
-
-      if not ssh-add -l | grep -q github
-          ssh-add ~/.ssh/github >/dev/null 2>&1
-      end
-    '';
-    };
-    git = {
-      enable = true;
-      userName = "vodchella";
-      userEmail = "twister.kz@gmail.com";
-    };
-    oh-my-posh = {
-      enable = true;
-      enableFishIntegration = true;
-      useTheme = "emodipt-extend";
-    };
-    home-manager.enable = true;
-    ssh.enable = true;
   };
 }
---
+++ programs/all.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./fish.nix
+  ];
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "vodchella";
+      userEmail = "twister.kz@gmail.com";
+    };
+    oh-my-posh = {
+      enable = true;
+      enableFishIntegration = true;
+      useTheme = "emodipt-extend";
+    };
+    home-manager.enable = true;
+    ssh.enable = true;
+  };
+}

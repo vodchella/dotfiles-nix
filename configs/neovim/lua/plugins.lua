6c30cfe39d4d9ba597ec9501380224dc100ea36b
vodchella <twister.kz@gmail.com>
2025-04-26 15:13:05 +0000

    Bufferline in nvim

--- configs/neovim/lua/plugins.lua
+++ configs/neovim/lua/plugins.lua
@@ -38,6 +38,14 @@ return require('packer').startup(function(use)
 
     -- Widgets
     use 'nvim-lualine/lualine.nvim'
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "*",
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+        config = function()
+            require('bufferline').setup({})
+        end
+    }
 
     -- Vim
     use {

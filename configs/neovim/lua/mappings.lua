6d1006c071cca49bd2acdfca2fb01405e8f92fc4
vodchella <twister.kz@gmail.com>
2025-04-26 21:11:26 +0000

    "Open file under cursor" function improved

--- configs/neovim/lua/mappings.lua
+++ configs/neovim/lua/mappings.lua
@@ -11,25 +11,5 @@ map('n', '<C-m>', ':Marks<CR>', opts)
 map('n', '<C-f>', ':Files<CR>', opts)
 map('n', '<C-b>', ':Buffers<CR>', opts)
 
--- Open file from grep-string
-map('n', '<C-g>', function()
-  local line = vim.fn.getline('.')
-  local filepath, lineno, colno = string.match(line, "([^:]+):(%d+):(%d+)")
-
-  if not filepath then
-    filepath, lineno = string.match(line, "([^:]+):(%d+)")
-    colno = 1
-  end
-
-  if filepath and lineno then
-    -- Проверяем, существует ли файл
-    if vim.fn.filereadable(filepath) == 1 then
-      vim.cmd('edit ' .. filepath)
-      vim.api.nvim_win_set_cursor(0, { tonumber(lineno), tonumber(colno) - 1 })
-    else
-      print('Файл не найден: ' .. filepath)
-    end
-  else
-    print("Не удалось распарсить строку")
-  end
-end, opts)
+local open_file = require('open_file')
+map('n', '<C-g>', open_file.open_file_under_cursor, opts)

56653c34cfb45f26a64cffba5a0e217f450ce841
vodchella <twister.kz@gmail.com>
2025-04-26 11:49:26 +0000

    Some keybindings for terminal in nvim

--- configs/neovim/lua/mappings.lua
+++ configs/neovim/lua/mappings.lua
@@ -1,7 +1,12 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- Terminal
+map('n', '<C-t>', ':terminal<CR>', opts)
+map('t', '<C-t>', [[<C-\><C-n>]], opts)
+
 -- FZF
 map('n', '<C-l>', ':Lines<CR>', opts)
 map('n', '<C-m>', ':Marks<CR>', opts)
 map('n', '<C-f>', ':Files<CR>', opts)
+map('n', '<C-b>', ':Buffers<CR>', opts)

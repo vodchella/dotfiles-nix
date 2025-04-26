a1d3706af4bd120d48a5487dcd27f762316927ef
vodchella <twister.kz@gmail.com>
2025-04-26 14:04:51 +0000

    Open files from grep-strings with ctrl+g

--- configs/neovim/lua/mappings.lua
+++ configs/neovim/lua/mappings.lua
@@ -1,5 +1,5 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
+local opts = { silent = true }
 
 -- Terminal
 map('n', '<C-t>', ':terminal<CR>', opts)
@@ -10,3 +10,26 @@ map('n', '<C-l>', ':Lines<CR>', opts)
 map('n', '<C-m>', ':Marks<CR>', opts)
 map('n', '<C-f>', ':Files<CR>', opts)
 map('n', '<C-b>', ':Buffers<CR>', opts)
+
+-- Open file from grep-string
+map('n', '<C-g>', function()
+  local line = vim.fn.getline('.')
+  local filepath, lineno, colno = string.match(line, "([^:]+):(%d+):(%d+)")
+
+  if not filepath then
+    filepath, lineno = string.match(line, "([^:]+):(%d+)")
+    colno = 1
+  end
+
+  if filepath and lineno then
+    -- Проверяем, существует ли файл
+    if vim.fn.filereadable(filepath) == 1 then
+      vim.cmd('edit ' .. filepath)
+      vim.api.nvim_win_set_cursor(0, { tonumber(lineno), tonumber(colno) - 1 })
+    else
+      print('Файл не найден: ' .. filepath)
+    end
+  else
+    print("Не удалось распарсить строку")
+  end
+end, opts)

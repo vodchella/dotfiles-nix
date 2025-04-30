2887f8cb3f60b46f50c6454ac8c8c5d3fb31f707
vodchella <twister.kz@gmail.com>
2025-04-30 17:33:39 +0500

    Forgot semicolons

--- aliases.nix
+++ aliases.nix
@@ -3,8 +3,8 @@
 {
   home.shellAliases = {
     "cd.." = "cd ..";
-    a = /*fish*/ "atac -d (find ~/.config/atac -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)"
-    gp = /*fish*/ "cd (find ~/projects -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)"
+    a = /*fish*/ "atac -d (find ~/.config/atac -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)";
+    gp = /*fish*/ "cd (find ~/projects -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)";
     hms="home-manager switch";
     r = /*fish*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
     v="nvim";

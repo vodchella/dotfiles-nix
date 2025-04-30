36108a73846b4bd235c5946dac4f722163c7c2a4
vodchella <twister.kz@gmail.com>
2025-04-30 17:31:28 +0500

    New aliases

--- aliases.nix
+++ aliases.nix
@@ -3,8 +3,10 @@
 {
   home.shellAliases = {
     "cd.." = "cd ..";
+    a = /*fish*/ "atac -d (find ~/.config/atac -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)"
+    gp = /*fish*/ "cd (find ~/projects -mindepth 1 -maxdepth 1 -type d -not -path "*/.git*" | fzf)"
     hms="home-manager switch";
-    r = /*bash*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
+    r = /*fish*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
     v="nvim";
     vt="nvim -c terminal";
     z="zellij";

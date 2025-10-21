a9e12899213557fe35f7468db76ebb163eafbb11
vodchella <twister.kz@gmail.com>
2025-10-21 16:12:24 +0000

    Some `posting` settings

--- aliases.nix
+++ aliases.nix
@@ -3,10 +3,10 @@
 {
   home.shellAliases = {
     "cd.." = "cd ..";
-    # a = /*fish*/ "atac -d (find ~/.config/atac -mindepth 1 -maxdepth 1 -type d -not -path '*/.git*' | fzf)";
     gc = "cd ${configDir}";
     gp = /*fish*/ "cd (find ~/projects -mindepth 1 -maxdepth 1 -type d -not -path '*/.git*' | fzf)";
     hms="home-manager switch";
+    p = /*fish*/ "posting --collection (find ~/.config/posting/collections -mindepth 1 -maxdepth 1 -type d -not -path '*/.git*' | fzf)";
     r = /*fish*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
     t="tmux";
     v="nvim";

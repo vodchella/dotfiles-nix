8b63cfe45dc63b741978c10daa5298ecffcece4a
vodchella <twister.kz@gmail.com>
2025-11-18 18:49:13 +0000

    `fish`: make mode indicator more gruvbox-like

--- configs/fish/functions/fish_mode_prompt.fish
+++ configs/fish/functions/fish_mode_prompt.fish
@@ -1,15 +1,15 @@
 function fish_mode_prompt
     switch $fish_bind_mode
         case default
-            set_color --bold 98c379;  printf '[Normal]'
+            set_color --bold b8bb26;  printf '[Normal]'      # green
         case insert
-            set_color --bold 61afef;  printf '[Insert]'
+            set_color --bold 83a598;  printf '[Insert]'      # blue
         case visual
-            set_color --bold c678dd;  printf '[Visual]'
+            set_color --bold d3869b;  printf '[Visual]'      # purple
         case replace_one
-            set_color --bold e06c75;  printf '[Replace]'
+            set_color --bold fb4934;  printf '[Replace]'     # red
         case '*'
-            set_color --bold a0a0a0;  printf '[?]'
+            set_color --bold a89984;  printf '[?]'           # fg2
     end
     set_color normal
 end

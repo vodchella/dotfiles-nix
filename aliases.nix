f7053140ee50ed53f03cd4ed0b7457ecad66f36e
vodchella <twister.kz@gmail.com>
2025-12-02 20:19:28 +0000

    `fish`: add aliase for `nix develop`

--- aliases.nix
+++ aliases.nix
@@ -7,10 +7,11 @@
     gp  = /*fish*/ "cd (find ~/projects -mindepth 1 -maxdepth 1 -type d -not -path '*/.git*' | fzf)";
     hms = "home-manager switch";
     lg  = "lazygit";
+    nd  = /*fish*/ "nix develop --command fish";
     p   = "run-posting";
     r   = /*fish*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
     t   = "tmux";
     v   = "nvim";
-    vt  = "nvim -c terminal";
+    vt  = /*fish*/ "nvim -c terminal";
   };
 }

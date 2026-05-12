d3692fd39742e5581bd3e23bbc119eabc7765ad3
vodchella <twister.kz@gmail.com>
2026-05-12 08:45:56 +0000

    `nvim`: new keybinding for `vim-easy-align`

--- configs/neovim/lua/mappings.lua
+++ configs/neovim/lua/mappings.lua
@@ -25,9 +25,9 @@ map('n', '<C-f>', ':Files<CR>', opts)
 map('n', '<C-b>', ':Buffers<CR>', opts)
 
 -- Easy align
-map('n', '<C-b>', ':Buffers<CR>', opts)
 map('x', 'ga', '<Plug>(EasyAlign)')
 map('n', 'ga', '<Plug>(EasyAlign)')
+map('v', 'gA', ':EasyAlign //<Left>')
 
 -- Database
 map('n', '<leader>qq', ':DBUIToggle<CR>', opts)

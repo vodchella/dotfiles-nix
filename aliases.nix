1e80e2312f8f14c9455ea62c7a64c051ccbbcefe
vodchella <twister.kz@gmail.com>
2026-06-21 18:11:00 +0000

    `fish`: alias for `serpl`

--- aliases.nix
+++ aliases.nix
@@ -10,6 +10,7 @@
     nd  = /*fish*/ "nix develop --command fish";
     p   = "run-posting";
     r   = /*fish*/ "ranger --choosedir=$HOME/.config/ranger/.rangerdir; cd (cat $HOME/.config/ranger/.rangerdir)";
+    s   = "serpl";
     v   = "nvim";
     vv  = "nvr -s";
     vr  = "nvr";

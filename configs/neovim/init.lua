46a1ba911d282a85bd8edee365b7c05718955749
vodchella <twister.kz@gmail.com>
2026-07-06 18:22:25 +0000

    `nvim`: some fixes after migrate to `vim.pack`

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -1,12 +1,12 @@
-require('cmp-conf')
-require("mappings")
+-- MUST be the first:
 require("plugins")
+
 require("settings")
+require("mappings")
 require("statusline")
 require("treesitter")
 require("colors")
-
--- MUST be the last:
+require('cmp-conf')
 require("user-cmds")
 require("auto-cmds")
 require("dap-conf")
--- configs/neovim/lua/cmp-conf.lua
+++ configs/neovim/lua/cmp-conf.lua
@@ -1,27 +1,14 @@
 local cmp_status_ok, cmp = pcall(require, 'cmp')
 if not cmp_status_ok then
+    vim.notify('nvim-cmp config is not ok', vim.log.levels.WARN)
     return
 end
 
--- local snip_status_ok, luasnip = pcall(require, "luasnip")
--- if not snip_status_ok then
---     return
--- end
-
--- require("luasnip/loaders/from_vscode").lazy_load()
-
 cmp.setup {
-    -- snippet = {
-    --     expand = function(args)
-    --         luasnip.lsp_expand(args.body)
-    --     end,
-    -- },
     sources = {
         { name = 'buffer' },
         { name = 'path' },
         { name = 'nvim_lsp' },
-        -- { name = 'nvim_lua' },
-        -- { name = 'luasnip' },
     },
     mapping = {
         ['<CR>'] = cmp.mapping.confirm { select = true },

ea74098934feaedf7ce51fd984d0c01de3a88d61
vodchella <twister.kz@gmail.com>
2026-07-06 17:38:36 +0000

    `nvim`: turn on `_core.ui2`

--- configs/neovim/lua/settings.lua
+++ configs/neovim/lua/settings.lua
@@ -1,3 +1,5 @@
+require('vim._core.ui2').enable()
+
 local set = vim.opt
 
 set.langmap        = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'

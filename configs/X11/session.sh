fa5adefcc94b327fc42ddc1b8675257b3ebacf11
vodchella <twister.kz@gmail.com>
2026-07-20 21:12:23 +0000

    `i3`: fixes

--- configs/X11/session.sh
+++ configs/X11/session.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-exec dbus-run-session sh -c '
-    xxkb &
-    pipewire &
-    wireplumber &
-    setxkbmap \
-        -layout us,ru \
-        -option grp:alt_space_toggle \
-        -option ctrl:nocaps
-'
+xxkb &
+pipewire &
+wireplumber &
 
+setxkbmap \
+    -layout us,ru \
+    -option grp:alt_space_toggle \
+    -option ctrl:nocaps
